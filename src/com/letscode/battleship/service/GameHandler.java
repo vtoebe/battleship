6dcf8aa01d9c7e7815d1b276ede42a05f3a6ff70
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-22 20:42:17 -0300

    feat: add final score
    added classes to get winner and show final score

--- src/com/letscode/battleship/service/GameHandler.java
+++ src/com/letscode/battleship/service/GameHandler.java
@@ -9,12 +9,15 @@ import com.letscode.battleship.utils.Writer;
 import java.util.Objects;
 
 public class GameHandler {
+    private static Player player1;
+    private static Player player2;
 
-    public static void runGame(Player player1, Player player2){
-        int rounds = 5;
+    public void runGame(Player player1, Player player2){
+        GameHandler.player1 = player1;
+        GameHandler.player2 = player2;
+        int rounds = 0;
 
-        while (rounds >= 0){
-            //TODO: handle invalid characters
+        while (rounds <= 5){
             System.out.print(player1.getName() + " | ");
             Printer.requestCoordinates();
             updateBoard(Writer.getCoordinates(), player2.board, player1);
@@ -28,15 +31,14 @@ public class GameHandler {
                 updateBoard(Writer.getCoordinates(), player1.board, player2);
             }
 
-            System.out.println("Rounds left: " + rounds);
-            System.out.println(player1);
-            System.out.println(player2);
-            rounds--;
+            System.out.println("Round " + rounds + "/5");
+            if (rounds < 5){
+                System.out.println(player1);
+                System.out.println(player2);
+            }
+            rounds++;
         }
-
-        System.out.println("End of the game!");
-        Printer.printBoard(player1.board.getGrids(), player1.getName());
-        Printer.printBoard(player2.board.getGrids(), player2.getName());
+        finalScore();
     }
 
     public static void updateBoard(int[] positions, Board opponentBoard, Player player) {
@@ -48,6 +50,39 @@ public class GameHandler {
         } else if (opponentBoard.getGrids()[x][y] == ' ') {
             opponentBoard.getGrids()[x][y] = BoardSymbols.MISS.getBoardsymbol();
             player.addMiss();
+        }
+    }
+
+    public static void finalScore(){
+        System.out.println("End of the Game!");
+
+        if (getWinner() != null  && !Objects.equals(getWinner().getName(), "Computer")){
+            System.out.println("Congratulations " + getWinner().getName() + "! You won!");
+        } else if (getWinner() != null && Objects.equals(getWinner().getName(), "Computer")){
+            System.out.println("The " + getWinner().getName() + " won!");
+        }else {
+            System.out.println("It was a tie!");
+        }
+        System.out.println(Printer.STATS);
+        System.out.println(player1);
+        System.out.println(player2);
+        Printer.printFinalBoards(player1, player2);
+
+    }
+
+    public static Player getWinner(){
+        if (player2.getHits() == player1.getHits()){
+            player1.setTies(1);
+            player2.setTies(1);
+            return null;
+        } else if (player1.getHits() > player2.getHits()){
+            player1.setWins(1);
+            player2.setLosses(1);
+            return player1;
+        } else{
+            player2.setWins(1);
+            player1.setLosses(1);
+            return player2;
         }
     }
 
