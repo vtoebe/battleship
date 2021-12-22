e8fcab86adeda2d0f505b969066280bace230ea6
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-21 23:14:04 -0300

    update: menu enum

---
+++ src/com/letscode/battleship/enums/Menu.java
@@ -0,0 +1,18 @@
+package com.letscode.battleship.enums;
+
+public enum Menu {
+    START("Start New Game"),
+    TUTORIAL("Tutorial"),
+    ABOUT("About"),
+    END_GAME("End Battleship");
+
+    private final String description;
+
+    Menu(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+}
