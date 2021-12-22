9d662e4885e5ccfa5bb5f18d7bc4fd913fe6862b
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-22 20:42:43 -0300

    refactor: add wins, losses and ties

--- src/com/letscode/battleship/entities/Player.java
+++ src/com/letscode/battleship/entities/Player.java
@@ -4,6 +4,10 @@ public class Player {
     private final String name;
     private int hits;
     private int misses;
+    private int wins;
+    private int losses;
+    private int ties;
+
 
     public Board board = new Board();
     public Player (String name){
@@ -14,10 +18,21 @@ public class Player {
     public void addHit() {
         hits++;
     }
+    public int getHits(){ return hits; }
 
     public void addMiss() {
         misses++;
     }
+//    public int getMisses(){ return misses; }
+
+    public void setWins(int wins) { this.wins += wins; }
+//    public int getWins() { return wins; }
+
+    public void setLosses(int losses) { this.losses += losses; }
+//    public int getLosses() { return losses; }
+
+    public void setTies(int ties) { this.ties += ties; }
+//    public int getTies() { return ties; }
 
     @Override
     public String toString() {
