e96d53885faab811071f845b675c0b9768492d48
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-21 22:53:44 -0300

    update: board

---
+++ src/com/letscode/battleship/entities/Board.java
@@ -0,0 +1,36 @@
+package com.letscode.battleship.entities;
+
+import java.util.Arrays;
+
+public class Board {
+    private final char[][] grids = new char[10][10];
+
+    public Board() {
+        this.init();
+        this.placeShips();
+
+    }
+
+    // Inicializando o tabuleiro com espaços vazios
+    public void init() {
+        for (char[] grid : grids) {
+            Arrays.fill(grid, ' ');
+        }
+    }
+
+    // Posicionar os navios no tabuleiro
+    public void placeShips() {
+        for (int i = 0; i < grids.length; i++) {
+            int x = (int) (Math.random() * 10);
+            int y = (int) (Math.random() * 10);
+
+            if(grids[x][y] != 'S'){
+                grids[x][y] = 'S';
+            } else { i--; }
+        }
+    }
+
+    public char[][] getGrids() {
+        return grids;
+    }
+}
