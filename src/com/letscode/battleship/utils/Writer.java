7f95686e5dbe341c56365d449795b00738610b89
Marisa Sales <marisa_sales6@hotmail.com>
2021-12-22 23:29:55 -0300

    feat: won't generate random repeated coordinates
    added feat so user input is always lower case

--- src/com/letscode/battleship/utils/Writer.java
+++ src/com/letscode/battleship/utils/Writer.java
@@ -2,6 +2,7 @@ package com.letscode.battleship.utils;
 
 import com.letscode.battleship.entities.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -13,6 +14,7 @@ public class Writer {
     private static Player player1;
     private static Player player2;
     static int[] coordinates = new int[2];
+    static ArrayList<ArrayList<int[]>> computerCoordinates = new ArrayList<ArrayList<int[]>>();
 
     public static void setPlayer1() { player1 = new Player(scan.next()); }
     public static Player getPlayer1() { return player1; }
@@ -35,14 +37,20 @@ public class Writer {
     }
 
     public static int[] getRandomCoordinates() {
-        int x = (int) (Math.random() * 10);
-        int y = (int) (Math.random() * 10);
-        return new int[] {x, y};
+        ArrayList<int[]> coord = new ArrayList<int[]>();
+        do {
+            coordinates[0] = (int) (Math.random() * 10);
+            coordinates[1] = (int) (Math.random() * 10);
+            System.out.println(Arrays.toString(coordinates));
+            coord.add(coordinates);
+        } while (computerCoordinates.contains(coord));
+        computerCoordinates.add(coord);
+        return coordinates;
     }
 
     public static boolean getLine() {
         final String[] LINE_LETTERS = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        String line = scan.next();
+        String line = scan.next().toLowerCase();
 
         if (Arrays.asList(LINE_LETTERS).contains(line)) {
             coordinates[0] = charToInt(line);
