28a5a649fc7032e993eb4b32f8ce8bf0068893cc
Marisa Sales <marisa_sales6@hotmail.com>
2021-12-22 23:25:22 -0300

    refactor: update how boards are shown
    • updated boards so they'll show colors, lines and columns
    • changed separator variable so it's dinamic

--- src/com/letscode/battleship/utils/Printer.java
+++ src/com/letscode/battleship/utils/Printer.java
@@ -1,6 +1,7 @@
 package com.letscode.battleship.utils;
 
 import com.letscode.battleship.entities.Player;
+import com.letscode.battleship.enums.BoardSymbols;
 import com.letscode.battleship.enums.Menu;
 
 import static com.letscode.battleship.utils.Formatter.*;
@@ -13,6 +14,7 @@ public class Printer {
     private static final String OPPONENT_SELECTION = "Select your opponent:";
     private static final String MENU_SELECTION = "Your selection: ";
     public static final String INVALID_CHOICE = "Invalid selection! Please try again.";
+    public static final String INVALID_COORD = "Coordinates already entered, please choose another one.";
 
     private static final String REQUEST_COORDINATES = "Select a position to shoot";
     private static final String REQUEST_LINE = "Line (A-J): ";
@@ -24,24 +26,24 @@ public class Printer {
     }
 
     public static void gameInitialMenu() {
-        System.out.println(SEPARATOR);
-        System.out.println(ANSI_CYAN + center(BATTLESHIP_NAME, SEPARATOR.length()) + ANSI_RESET);
-        System.out.println(SEPARATOR);
-        System.out.println(ANSI_CYAN + center(MENU_OPTION, SEPARATOR.length()) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(BATTLESHIP_NAME, 45) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(MENU_OPTION, 45) + ANSI_RESET);
         System.out.println(right("[1] " + Menu.START.getDescription()));
         System.out.println(right("[2] " + Menu.TUTORIAL.getDescription()));
         System.out.println(right("[3] " + Menu.ABOUT.getDescription()));
         System.out.println(right("[4] " + Menu.END_GAME.getDescription()));
-        System.out.println(SEPARATOR);
+        System.out.println(SEPARATOR.repeat(45));
         System.out.print(MENU_SELECTION);
     }
 
     public static void opponentSelectionMenu(){
-        System.out.println(SEPARATOR);
-        System.out.println(ANSI_CYAN + center(OPPONENT_SELECTION, SEPARATOR.length()) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(OPPONENT_SELECTION, 45) + ANSI_RESET);
         System.out.println(right("[1] Player 2"));
         System.out.println(right("[2] Machine"));
-        System.out.println(SEPARATOR);
+        System.out.println(SEPARATOR.repeat(45));
         System.out.print(MENU_SELECTION);
     }
 
@@ -58,26 +60,47 @@ public class Printer {
     }
 
     public static void printBoard(char[][] board, String player) {
+        // Title
         System.out.println();
-        System.out.println(SEPARATOR);
-        System.out.println(ANSI_CYAN + center(player, SEPARATOR.length()) + ANSI_RESET);
-        System.out.println(SEPARATOR);
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(player, 45) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
 
-        for (char[] grid : board) {
-            for (char value : grid) {
-                System.out.printf("| %s ", value);
+        // COLUMN_POSITION
+        for (char value : COLUMN_POSITION) {
+            System.out.printf("| " + ANSI_CYAN + "%s " + ANSI_RESET, value);
+        }
+        System.out.println("|");
+        System.out.println(SEPARATOR.repeat(45));
+
+        for (int line = 0; line < board.length; line++) {
+            // ROW_POSITION
+            System.out.printf("| " + ANSI_CYAN + "%s " + ANSI_RESET, ROW_POSITION[line]);
+
+            // BOARD
+            for (int col = 0; col < board[line].length; col++) {
+                if (board[line][col] == BoardSymbols.HIT.getBoardsymbol()) {
+                    System.out.printf("| " + ANSI_GREEN + "%s " + ANSI_RESET, board[line][col]);
+                } else if (board[line][col] == BoardSymbols.MISS.getBoardsymbol()) {
+                    System.out.printf("| " + ANSI_RED + "%s " + ANSI_RESET, board[line][col]);
+                } else {
+                    System.out.printf("| %s ", board[line][col]);
+                }
             }
             System.out.println("|");
-            System.out.println(SEPARATOR);
+            System.out.println(SEPARATOR.repeat(45));
         }
     }
 
     public static void printFinalBoards(Player player1, Player player2){
-        System.out.println(SEPARATOR + SEPARATOR);
-        System.out.print(ANSI_CYAN + center(player1.getName(), SEPARATOR.length()) + ANSI_RESET);
-        System.out.println(ANSI_CYAN + center(player2.getName(), SEPARATOR.length()*2) + ANSI_RESET);
-        System.out.println(SEPARATOR + SEPARATOR);
-          for (int line = 0; line < 10; line++){
+        System.out.println(SEPARATOR.repeat(41) + "        " + SEPARATOR.repeat(41));
+        System.out.print(ANSI_CYAN + center(player1.getName(), 41) + ANSI_RESET);
+        System.out.print("        ");
+        System.out.println(ANSI_CYAN + center(player2.getName(), 82) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(41) + "        " + SEPARATOR.repeat(41));
+
+        for (int line = 0; line < 10; line++){
+
               for (int col=0; col < 10; col++) {
                   System.out.printf("| %s ", player1.board.getGrids()[line][col]);
               }
@@ -87,7 +110,7 @@ public class Printer {
                   System.out.printf("| %s ", player2.board.getGrids()[line][colboard2]);
               }
               System.out.println("|");
-              System.out.println(SEPARATOR + SEPARATOR);
+              System.out.println(SEPARATOR.repeat(41) + "        " + SEPARATOR.repeat(41));
           }
     }
 }
