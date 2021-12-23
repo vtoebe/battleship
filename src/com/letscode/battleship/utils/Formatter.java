f6dc94c6a7059d98eae4fb5c7918621432d21ae6
Marisa Sales <marisa_sales6@hotmail.com>
2021-12-22 23:11:29 -0300

    refactor: change variables

--- src/com/letscode/battleship/utils/Formatter.java
+++ src/com/letscode/battleship/utils/Formatter.java
@@ -3,7 +3,11 @@ package com.letscode.battleship.utils;
 public class Formatter {
     static final String ANSI_RESET = "\u001B[0m";
     static final String ANSI_CYAN = "\u001B[36m";
-    static final String SEPARATOR = "---------------------------------------------";
+    static final String ANSI_RED = "\u001B[31m";
+    static final String ANSI_GREEN = "\u001B[32m";
+    static final String SEPARATOR = "-";
+    static final char[] COLUMN_POSITION = {' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    static final char[] ROW_POSITION = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     static String center(String text, int size) {
         int center = size / 2 - text.length() / 2;
@@ -11,7 +15,7 @@ public class Formatter {
     }
 
     static String right(String text) {
-        return String.format("%" + (16) + "s", "") + text;
+        return String.format("%" + (12) + "s", "") + text;
     }
 
     static int charToInt(String letter) {
