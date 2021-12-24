9bc9bb92a2ed48ea5ead1807e4570af2e37b9cbe
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-23 21:37:39 -0300

    feat: add menu input validation

--- src/com/letscode/battleship/service/MenuHandler.java
+++ src/com/letscode/battleship/service/MenuHandler.java
@@ -5,39 +5,58 @@ import com.letscode.battleship.utils.Printer;
 import com.letscode.battleship.utils.Writer;
 
 public class MenuHandler {
+    public static boolean isRunning = false;
+
+    public static void checkMenuSelection(){
+        do {
+            isRunning = true;
+            Printer.gameInitialMenu();
+            MenuHandler.checkMenu(Writer.menuSelection());
+        } while(isRunning);
+    }
+
+    public static void checkOpponentSelection(){
+        do {
+            Printer.opponentSelectionMenu();
+        } while(!MenuHandler.checkOpponent(Writer.menuSelection()));
+    }
 
     public static boolean checkMenu(int choice){
         switch (choice) {
             case 1:
                 Printer.requestName();
                 Writer.setPlayer1();
-                return true;
+                checkOpponentSelection();
+                return isRunning = true;
             case 2:
                 System.out.println(Menu.TUTORIAL.getDescription());
-                return true;
+                return isRunning = true;
             case 3:
                 System.out.println(Menu.ABOUT.getDescription());
-                return true;
+                return isRunning = true;
             case 4:
                 System.out.println(Menu.END_GAME.getDescription());
-                return false;
+                return isRunning = false;
             default:
                 System.out.println(Printer.INVALID_CHOICE);
-                return true;
+                return isRunning = true;
         }
     }
 
-    public static void checkOpponent(int choice){
+    public static boolean checkOpponent(int choice){
         switch (choice) {
             case 1:
                 Printer.requestName();
                 Writer.setPlayer2();
-                break;
+                GameHandler.runGame(Writer.getPlayer1(), Writer.getPlayer2());
+                return true;
             case 2:
                 Writer.setPlayer2("Computer");
-                break;
+                GameHandler.runGame(Writer.getPlayer1(), Writer.getPlayer2());
+                return true;
             default:
                 System.out.println(Printer.INVALID_CHOICE);
         }
+        return false;
     }
 }
