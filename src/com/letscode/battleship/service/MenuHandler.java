2f519f5e56e26f36ad41c60b7a40ba5d58eca27a
Marisa Sales <marisa_sales6@hotmail.com>
2021-12-23 23:51:20 -0300

    refactor: update check menu signature

--- src/com/letscode/battleship/service/MenuHandler.java
+++ src/com/letscode/battleship/service/MenuHandler.java
@@ -1,8 +1,8 @@
 package com.letscode.battleship.service;
 
 import com.letscode.battleship.enums.Menu;
-import com.letscode.battleship.utils.Printer;
-import com.letscode.battleship.utils.Writer;
+import com.letscode.battleship.utils.BattleshipPrinter;
+import com.letscode.battleship.utils.BattleshipWriter;
 
 public class MenuHandler {
     public static boolean isRunning = false;
@@ -10,52 +10,58 @@ public class MenuHandler {
     public static void checkMenuSelection(){
         do {
             isRunning = true;
-            Printer.gameInitialMenu();
-            MenuHandler.checkMenu(Writer.menuSelection());
+            BattleshipPrinter.gameInitialMenu();
+            MenuHandler.checkMenu(BattleshipWriter.menuSelection());
         } while(isRunning);
     }
 
     public static void checkOpponentSelection(){
         do {
-            Printer.opponentSelectionMenu();
-        } while(!MenuHandler.checkOpponent(Writer.menuSelection()));
+            BattleshipPrinter.opponentSelectionMenu();
+        } while(!MenuHandler.checkOpponent(BattleshipWriter.menuSelection()));
     }
 
-    public static boolean checkMenu(int choice){
+    public static void checkMenu(int choice){
         switch (choice) {
             case 1:
-                Printer.requestName();
-                Writer.setPlayer1();
+                BattleshipPrinter.requestName();
+                BattleshipWriter.setPlayer1();
                 checkOpponentSelection();
-                return isRunning = true;
+                isRunning = true;
+                break;
             case 2:
                 System.out.println(Menu.TUTORIAL.getDescription());
-                return isRunning = true;
+                FileHandler.readFile("tutorial.txt");
+                isRunning = true;
+                break;
             case 3:
                 System.out.println(Menu.ABOUT.getDescription());
-                return isRunning = true;
+                FileHandler.readFile("about.txt");
+                isRunning = true;
+                break;
             case 4:
                 System.out.println(Menu.END_GAME.getDescription());
-                return isRunning = false;
+                isRunning = false;
+                break;
             default:
-                System.out.println(Printer.INVALID_CHOICE);
-                return isRunning = true;
+                System.out.println(BattleshipPrinter.INVALID_CHOICE);
+                isRunning = true;
         }
     }
 
     public static boolean checkOpponent(int choice){
         switch (choice) {
             case 1:
-                Printer.requestName();
-                Writer.setPlayer2();
-                GameHandler.runGame(Writer.getPlayer1(), Writer.getPlayer2());
+                BattleshipPrinter.requestName();
+                BattleshipWriter.setPlayer2();
+                GameHandler.runGame(BattleshipWriter.getPlayer1(), BattleshipWriter.getPlayer2());
                 return true;
             case 2:
-                Writer.setPlayer2("Computer");
-                GameHandler.runGame(Writer.getPlayer1(), Writer.getPlayer2());
+                BattleshipWriter.setPlayer2("Computer");
+                GameHandler.runGame(BattleshipWriter.getPlayer1(), BattleshipWriter.getPlayer2());
                 return true;
             default:
-                System.out.println(Printer.INVALID_CHOICE);
+                System.out.println(BattleshipPrinter.INVALID_CHOICE);
         }
         return false;
     }
