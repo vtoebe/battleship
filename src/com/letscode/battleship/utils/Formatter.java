46b1d3f748190130a9591a1d9a35ae92c2bc06c1
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-23 21:35:34 -0300

    feat: add text format functions

--- src/com/letscode/battleship/utils/Formatter.java
+++ src/com/letscode/battleship/utils/Formatter.java
@@ -3,12 +3,15 @@ package com.letscode.battleship.utils;
 import com.letscode.battleship.entities.Player;
 import com.letscode.battleship.enums.BoardSymbols;
 
+import static com.letscode.battleship.utils.Printer.BATTLESHIP_NAME;
+import static com.letscode.battleship.utils.Printer.MENU_OPTION;
+
 public class Formatter {
     static final String ANSI_RESET = "\u001B[0m";
     static final String ANSI_CYAN = "\u001B[36m";
     static final String ANSI_RED = "\u001B[31m";
     static final String ANSI_GREEN = "\u001B[32m";
-    static final String SEPARATOR = "-";
+    public static final String SEPARATOR = "-";
     static final char[] COLUMN_POSITION = {' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     static final char[] ROW_POSITION = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
@@ -49,7 +52,7 @@ public class Formatter {
         }
     }
 
-    static void boardColor(Player player, int line) {
+    static void coloredBoard(Player player, int line) {
         for (int col = 0; col < 10; col++) {
             if (player.board.getGrids()[line][col] == BoardSymbols.HIT.getBoardsymbol()) {
                 System.out.printf("| " + ANSI_GREEN + "%s " + ANSI_RESET, player.board.getGrids()[line][col]);
@@ -59,5 +62,39 @@ public class Formatter {
                 System.out.printf("| %s ", player.board.getGrids()[line][col]);
             }
         }
+    }
+
+    static void boardLetters(int line){
+        System.out.printf("| " + ANSI_CYAN + "%s " + ANSI_RESET, ROW_POSITION[line]);
+    }
+
+    static void boardNumbers(){
+        for (char value : COLUMN_POSITION) {
+            System.out.printf("| " + ANSI_CYAN + "%s " + ANSI_RESET, value);
+        }
+        System.out.println("|");
+        System.out.println(SEPARATOR.repeat(45));
+    }
+
+    static void singlePlayerTopBoard(Player player){
+        System.out.println();
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(player.getName(), 45) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
+    }
+
+    static void finalBoardsTop(Player player1, Player player2){
+        System.out.println(SEPARATOR.repeat(41) + "        " + SEPARATOR.repeat(41));
+        System.out.print(ANSI_CYAN + center(player1.getName(), 41) + ANSI_RESET);
+        System.out.print("        ");
+        System.out.println(ANSI_CYAN + center(player2.getName(), 82) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(41) + "        " + SEPARATOR.repeat(41));
+    }
+
+    static void initialMenuTop(){
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(BATTLESHIP_NAME, 45) + ANSI_RESET);
+        System.out.println(SEPARATOR.repeat(45));
+        System.out.println(ANSI_CYAN + center(MENU_OPTION, 45) + ANSI_RESET);
     }
 }
