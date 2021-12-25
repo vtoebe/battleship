829ebc88e3858a31e924087a70189f27b3a8ded5
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-25 14:45:11 -0300

    feat(gameHandler): continue game
    • added function to continue current game
      - resets both player's previous match stats
      - runs game with the same players
    • updated get winner - now it updates local static variable winner
    • simplified final score conditions
    • added printer of both players game stats (win, losses, ties)

--- src/com/letscode/battleship/service/GameHandler.java
+++ src/com/letscode/battleship/service/GameHandler.java
@@ -12,9 +12,9 @@ import java.util.Objects;
 public class GameHandler {
     private static Player player1;
     private static Player player2;
+    static String winner;
 
     public static void runGame(Player player1, Player player2){
-
         GameHandler.player1 = player1;
         GameHandler.player2 = player2;
         int rounds = 1;
@@ -39,6 +39,12 @@ public class GameHandler {
             rounds++;
         }
         finalScore();
+    }
+
+    public static void continueGame(){
+        player1.resetMatchStats();
+        player2.resetMatchStats();
+        GameHandler.runGame(player1, player2);
     }
 
     private static void getUserCoordinates(Player player, Player opponent){
@@ -69,35 +75,37 @@ public class GameHandler {
         System.out.println(BattleshipFormatter.SEPARATOR.repeat(45));
         System.out.println("End of the Game!");
 
-        if (getWinner() != null  && !Objects.equals(getWinner().getName(), "Computer")){
-            System.out.println("Congratulations " + getWinner().getName() + "! You won!");
-        } else if (getWinner() != null && Objects.equals(getWinner().getName(), "Computer")){
-            System.out.println("The " + getWinner().getName() + " won!");
-        }else {
+        getWinner();
+        if (winner == null){
             System.out.println("It was a tie!");
+        } else if (!winner.equals("Computer")){
+            System.out.println("Congratulations " + winner + "! You won!");
+        } else {
+            System.out.println("The " + winner + " won!");
         }
+
         System.out.println(BattleshipFormatter.SEPARATOR.repeat(45));
         System.out.println(BattleshipPrinter.STATS);
         System.out.println(player1);
         System.out.println(player2);
         System.out.println(BattleshipFormatter.SEPARATOR.repeat(45));
-
+        System.out.println("Name: " + player1.getName() + " | Wins: " + player1.getWins() + " | Losses: " + player1.getLosses() + " | Ties: " + player1.getTies());
+        System.out.println("Name: " + player2.getName() + " | Wins: " + player2.getWins() + " | Losses: " + player2.getLosses() + " | Ties: " + player2.getTies());
     }
 
-    public static Player getWinner(){
+    public static void getWinner(){
         if (player1.getHits() == player2.getHits()){
             player1.setTies();
             player2.setTies();
-            return null;
+            winner = null;
         } else if (player1.getHits() > player2.getHits()){
             player1.setWins();
             player2.setLosses();
-            return player1;
+            winner = player1.getName();
         } else{
             player2.setWins();
             player1.setLosses();
-            return player2;
+            winner =  player2.getName();
         }
     }
-
 }
