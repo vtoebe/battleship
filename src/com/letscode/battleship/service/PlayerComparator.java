d24880e619f6a780c846530f6bbce86ff0a53259
Vitória Toebe <vitoriatoebe@gmail.com>
2022-01-05 17:32:23 -0300

    refactor(compare): comparison is now based on player's total score

--- src/com/letscode/battleship/service/PlayerComparator.java
+++ src/com/letscode/battleship/service/PlayerComparator.java
@@ -8,12 +8,6 @@ public class PlayerComparator implements Comparator<Player> {
 
     @Override
     public int compare(Player p1, Player p2) {
-        if (p2.getWins() == p1.getWins()){
-            if (p1.getTies() != p2.getTies()){
-                return p2.getTies() - p1.getTies();
-            }
-            return p1.getLosses() - p2.getLosses();
-        }
-        return p2.getWins() - p1.getWins();
+        return Double.compare(p2.getTotalScore(), p1.getTotalScore());
     }
 }
